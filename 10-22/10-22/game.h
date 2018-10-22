1cb000bbd517244db50f42186181e84adaf6fcae
dpfnmt <1097722509@qq.com>
2018-10-22 14:33:10 +0800

    提交代码

---
+++ 10-22/10-22/game.h
@@ -0,0 +1,18 @@
+#ifndef  __GAME_H__
+#define  __GAME_H__
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+#define ROW  5
+#define COL  5
+
+
+void InitBoard(char board[ROW][COL],int row, int col);
+void DispiayBoard(char board[ROW][COL],int row, int col);
+void ComputerMove(char board[ROW][COL],int row, int col);
+void PlayerMove(char board[ROW][COL],int row, int col);
+char Iswin(char board[ROW][COL],int row, int col);
+int IsFull(char board[ROW][COL], int row, int col);
+#endif // __GAME_H__
---
+++ 10-22/10-22/test.c
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "game.h"
+
+void  menu()
+{
+   printf("********************************************\n");
+   printf("*********            1.play         ********\n");
+   printf("*********            0.exit         ********\n");
+   printf("********************************************\n");
+}
+
+void  game()
+{
+	char board[ROW][COL]={0};
+	char ret =0;
+
+	InitBoard(board,ROW,COL);
+	DispiayBoard(board,ROW,COL);
+
+	while(1)
+	{
+	  ComputerMove(board,ROW,COL);
+	  ret=Iswin(board,ROW,COL);
+	  if(ret != ' ')
+		  break;
+	  DispiayBoard(board,ROW,COL);
+	  PlayerMove(board,ROW,COL);
+	  ret=Iswin(board,ROW,COL);
+	  if(ret != ' ')
+		  break;
+	  DispiayBoard(board,ROW,COL);
+	}
+	if(ret == 'X')
+	{
+		printf("电脑赢\n");
+	}
+	else if(ret == '0')
+	{
+		printf("玩家赢\n");
+	}
+	else if(ret == 'Q')
+	{
+		printf("平局\n");
+	}
+	
+DispiayBoard(board,ROW,COL);
+}
+int main()
+{
+   int input=0;
+   srand((unsigned)time(NULL));
+	do
+	{
+	 menu();
+	 printf("请选择:>");
+	 scanf("%d",&input);
+	 switch(input)
+	 {
+	 case 1:
+		 game();
+		 break;
+	 case 0:
+		 printf("退出游戏\n");
+		 break;
+	 default:
+		 printf("输入错误，请重新输入\n");
+		 break;
+	 }
+	}while(input);
+  return 0;
+}
