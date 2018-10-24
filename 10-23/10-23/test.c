8f870ffeffb954b221809b54c9d4cb4fdca500ae
dpfnmt <1097722509@qq.com>
2018-10-24 22:59:56 +0800

    提交代码

---
+++ 10-23/10-23/test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int fic(int n)
+//{
+//     if(n <= 2)
+//	     return 1;
+//      else
+//	      return fic(n-1)+fic(n-2);
+//}
+//int main()
+//{
+//  int n = 0;
+//  int ret = 0;
+//  scanf("%d",&n);
+//   ret =fic(n);
+//   printf("%d\n",ret);
+//   return 0;
+//}
+
+
+
+//int fic(int n)
+//{
+//    int a=1;
+//	int b=1;
+//	int c=0;
+//	if(n<=2)
+//		return 1;
+//	else
+//	{
+//		int i=0;
+//        for(i=2;i<n;i++)
+//		   {
+//		      c = a+b;
+//		      a = b;
+//		      b = c;
+//			 
+//		    }	  
+//	}
+//		return c;
+//}
+//int main()
+//{
+//  int n = 0;
+//  scanf("%d",&n);
+//   printf("%d\n",fic(n));
+//   return 0;
+//}
+
