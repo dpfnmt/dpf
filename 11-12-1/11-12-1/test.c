b19747fce98c370122c4a25454c56cb88cc77ae9
dpfnmt <1097722509@qq.com>
2018-11-12 17:29:05 +0800

    提交代码

---
+++ 11-12-1/11-12-1/test.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int i,j,k,l,n;
+//	scanf("%d",&n);
+//	for(i=1;i<n;i++)
+//	{
+//	    for(j=1;j<n-i;j++)
+//		{
+//		   printf(" ");
+//		}
+//		for(k=1;k<i+1;k++)
+//		{
+//		  printf("%d",k);
+//
+//		}
+//		for(l=i-1;l>=1;l--)
+//		{
+//		   printf("%d",l);
+//		 }
+//		printf("%\n");
+//	}
+//   return 0;
+//}
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{ 
+//	double i,j,k,s,n;
+//	
+//	scanf("%d",&n);
+//	for(i=1;i<n;i++)
+//	{
+//		//k=2*((i+3)/(i+2));
+//	    printf("k=%l",k=2*((i+3)/(i+2)));
+//	 }
+//   return 0;
+//}
+//
+//#include<stdio.h>
+//main()
+//{
+//int n,term;
+//float PI=1.0;
+//for(n=2;n<=100;n=n+2)
+//{
+//term=n*n/((n+1)*(n-1));
+//PI=PI*term;
+//}
+//printf("PI=%f",PI*2);
+//}
