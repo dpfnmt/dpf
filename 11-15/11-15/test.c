80999146a642aa75ab203de4b84e7170878c39a9
dpfnmt <1097722509@qq.com>
2018-11-17 22:52:14 +0800

    提交代码

---
+++ 11-15/11-15/test.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a[]={1,2,3,4};
+//    
+//	printf("%d\n",sizeof(a));   //16
+//	printf("%d\n",sizeof(a+0));  //4
+//	printf("%d\n",sizeof(*a));   //4
+//	printf("%d\n",sizeof(a+1));  //4
+//	printf("%d\n",sizeof(a[1])); //4
+//	printf("%d\n",sizeof(&a));//4
+//	printf("%d\n",sizeof(*&a));//16
+//	printf("%d\n",sizeof(&a+1));//4
+//	printf("%d\n",sizeof(&a[0]));//4
+//	printf("%d\n",sizeof(&a[0]+1));//4
+//   return 0;
+//}
+
+//int main()
+//{
+//	int a[3][4]={0};
+//	int *p = NULL;
+//	printf("%d\n",sizeof(a));//48 4 16 16 0 4 0 4 16     
+//	printf("%d\n",sizeof(a[0][0]));
+//	printf("%d\n",sizeof(a[0]));
+//	printf("%d\n",sizeof(a[0]+1)); //4
+//	printf("%d\n",sizeof(&a[0][0]));
+//	printf("%d\n",sizeof(a+1));
+//	printf("%d\n",sizeof(*(a[0]+1)));
+//	printf("%d\n",sizeof(a+1));
+//	printf("%d\n",sizeof(*(a+1))); //16
+//	printf("%d\n",sizeof(&a[0]+1));
+//	printf("%d\n",sizeof(*(&a[0]+1)))7777777;
+//	printf("%d\n",sizeof(*a));
+//	printf("%d\n",sizeof(a[3]));
+//    
+//   return 0;
+//}
+
+int main()
+{
+
+   return 0;
+}
