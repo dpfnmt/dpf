e2f0b5cb0740fa63fee6fddc012ae37f87fde681
dpfnmt <1097722509@qq.com>
2019-01-19 15:34:51 +0800

    排序的代码

---
+++ 排序/排序/Sort.h
@@ -0,0 +1,30 @@
+#ifndef    __SORT_H__
+#define    __SORT_H__
+
+
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<assert.h> 
+#include<malloc.h> 
+#include<time.h> 
+#include<memory.h>
+
+
+void InsertSort(int* a , int n);
+void ShellSort(int* a , int n);
+void SelecSort(int* a , int n);
+void HeapSort(int* a, int n);
+
+//½»»»ÅÅÐò
+//Ã°ÅÝÅÅÐò£»
+void BubbleSort(int* a , int n);
+void QuickSort(int* a , int left , int right);
+void MergeSort(int* a, int n);
+
+
+
+void BenchMank(int* a , int n);//¼ìÑé¿ìÂý£»
+void PrintArray(int* a , int n);
+
+
+#endif
