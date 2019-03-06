70b7e5a2275d2dff3ba9a5a246dbb64eb8ceb02a
dpfnmt <1097722509@qq.com>
2019-03-06 18:34:37 +0800

    C语言的复习

---
+++ 3-6（C语言的复习）/3-6（C语言的复习）/test.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//int main()
+//{
+//	printf("hello word\n");
+//	printf("hello word\n");
+//	printf("hello word\n");
+//	printf("hello word\n");
+//
+//   return 0;
+//}
+
+//int main()
+//{
+//	int num = 10;
+//	scanf("%d",&num);
+//	printf("%d\n",num);
+//   return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n",sizeof(char));
+//   return 0;
+//}
+
+//当一个局部变量和全局变量的名字冲突的时候，先使用局部变量，如果局部变量不存在的时候，就使用全局变量。
